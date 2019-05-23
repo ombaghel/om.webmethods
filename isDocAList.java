5e20933ba1ad92de47b2d2d21c66c664f76aef8e
Om <48305271+ombaghel@users.noreply.github.com>
2019-05-22 20:09:03 -0400

    Create isDocAList.java

---
+++ isDocAList.java
@@ -0,0 +1,7 @@
+// documentList
+		IData[]	documentList = IDataUtil.getIDataArray( pipelineCursor, "inputDoc" );
+		if ( documentList != null) {
+			isList="true";		}
+		
+		// pipeline
+		IDataUtil.put( pipelineCursor, "isList", isList );
