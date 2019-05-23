aafd2dcd4ceeeff8b46f3ce40819b09db5ca040e
Om <48305271+ombaghel@users.noreply.github.com>
2019-05-22 20:44:20 -0400

    Update isInjectionPossible.java

--- isInjectionPossible.java
+++ isInjectionPossible.java
@@ -1,7 +1,7 @@
 		String isInjectionPossible="FALSE";
 		String s = "ALLOCATE CLOSE DECLARE DESCRIBE EXECUTE FETCH FREE GET INCLUDE OPEN PREPARE SET WHENEVER EXEC ALTER DROP CREATE SHUTDOWN 1=1";
 		String[] words = s.split("\\s+");
-	    List<String> tokens = Arrays.asList(words);
+	    	List<String> tokens = Arrays.asList(words);
 		String patternString = "\\b(" + StringUtils.join(tokens, "|") + ")\\b";
 		Pattern pattern = Pattern.compile(patternString);
 		Matcher matcher = pattern.matcher(sqlSentence.toUpperCase());
