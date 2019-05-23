139ec9f8353a10e4ffacba9e53aaa14873211427
Om <48305271+ombaghel@users.noreply.github.com>
2019-05-22 20:38:01 -0400

    Create GuessWhatIAmDoing.java

---
+++ GuessWhatIAmDoing.java
@@ -0,0 +1,32 @@
+ 				  IData[] aryData = IDataUtil.getIDataArray(cursorIn, "inputList");
+					IData outputDoc = IDataFactory.create();
+					IDataCursor outputDocCursor = outputDoc.getCursor();
+					
+		        for (int i = 0; i < aryData.length; i++) {
+		        	  
+		        	  String pKey=null;
+		        	  String[] pValue=null;
+		      		IDataCursor idc = aryData[i].getCursor();
+		      		while (idc.next())
+		      	  {
+		      		  String key = idc.getKey();
+		              if(key.contains("key"))
+		              {
+			               pKey = idc.getValue().toString();
+		
+		              }
+		              if(key.contains("values"))
+		              {
+		            	  pValue=IDataUtil.getStringArray(idc, "values");
+		
+		              }
+		      	  }
+		              
+			            IDataUtil.put(outputDocCursor, pKey, pValue);
+		
+			            idc.destroy();         
+		
+						
+					}
+       IDataUtil.put(cursorIn, "output", outputDoc);
+		
