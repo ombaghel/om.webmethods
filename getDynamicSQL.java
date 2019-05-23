5f9916c67cb593aa8abcf2dba3e4f7803a5330e2
Om <48305271+ombaghel@users.noreply.github.com>
2019-05-22 20:53:41 -0400

    Create getDynamicSQL.java

---
+++ getDynamicSQL.java
@@ -0,0 +1,9 @@
+String operation, String TABLENAME, String[] COLUMNS;
+StringBuilder sb = new StringBuilder();
+		if ("INSERT".equals(operation)) {
+		} else if ("UPDATE".equals(operation)) {
+		} else if ("SELECT".equals(operation)) {
+			else if ("DELETE".equals(operation)) {
+    } else {}
+    
+    //do the rest or approach me :) 
