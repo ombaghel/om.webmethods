45569f61b2313a7363f273d613438bbbe2817925
Om <48305271+ombaghel@users.noreply.github.com>
2019-05-22 20:06:56 -0400

    Create getValueForKey.java

---
+++ getValueForKey.java
@@ -0,0 +1,9 @@
+if ( inputDoc != null) {
+					  IDataCursor idc = inputDoc.getCursor();
+		 
+		        	  pValue=IDataUtil.getString(idc,inputKey);
+		 
+		             IDataUtil.put(pipelineCursor,  "inputKey", pValue);
+		
+		            idc.destroy();         
+		}
